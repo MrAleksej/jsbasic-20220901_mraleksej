461eb95421a9139709642b46324c893a939bd1f0
MrAleksej <lord_aleksej@mail.ru>
2022-11-14 15:20:21 +0300

    Решение задач блÐмодуля 5

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,31 @@
 function initCarousel() {
   // ваш код...
+  const carousel = document.querySelector('.carousel__inner')
+  const carouselWidth = carousel.offsetWidth
+  const nextButton = document.querySelector('.carousel__arrow_right')
+  const prevButton = document.querySelector('.carousel__arrow_left')
+  let countClick = 0
+
+  prevButton.style.display = 'none'
+  nextButton.addEventListener('click', () => {
+    countClick = ++countClick
+    carousel.style.transform = `translateX(-${carouselWidth*countClick}px)`
+    if(countClick === 3) {
+      nextButton.style.display = 'none'
+    } else {
+      nextButton.style.display = ''
+      prevButton.style.display = ''
+    }
+  })
+
+  prevButton.addEventListener('click', () => {
+    countClick = --countClick
+    carousel.style.transform = `translateX(-${carouselWidth*(countClick)}px)`
+    if(countClick === 0) {
+      prevButton.style.display = 'none'
+    } else {
+      prevButton.style.display = ''
+      nextButton.style.display = ''
+    }
+  })
 }
